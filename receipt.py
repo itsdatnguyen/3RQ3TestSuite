bed45d7826bb162e3c34a3f2d6142365555b89a2
Dat Nguyen <itsdatnguyen@gmail.com>
2020-12-02 14:36:49 -0500

    Fixed some syntax errors that caused tests to fail

--- receipt.py
+++ receipt.py
@@ -1,4 +1,4 @@
-class Order:
+class Receipt:
     def __init__(self, order):
         self.order = order
         self.content = None
--- test_email.py
+++ test_email.py
@@ -16,21 +16,21 @@ STAFF_ACCOUNT = Account(STAFF_EMAIL)
 GUEST_ACCOUNT = Account(GUEST_EMAIL)
 
 
-def test_user_has_account(self):
+def test_user_has_account():
     # 4.4 Existing and Guest User Receipt
 
     assert emailUtils.account_exist(
         GUEST_EMAIL) == False, 'Account does not exist'
 
 
-def test_user_does_not_have_account(self):
+def test_user_does_not_have_account():
     # 4.4 Existing and Guest User Receipt
 
     assert emailUtils.account_exist(
         EXISTING_ACCOUNT_EMAIL) == True, 'Account does exist'
 
 
-def test_send_receipt_to_guest(self):
+def test_send_receipt_to_guest():
     # 4.4.1 Guest User Receipt
     new_order = Order(GUEST_ACCOUNT, STAFF_ACCOUNT)
 
@@ -43,7 +43,7 @@ def test_send_receipt_to_guest(self):
         GUEST_EMAIL, order_receipt) == True, 'Email has been sent'
 
 
-def test_send_receipt_to_existing_account(self):
+def test_send_receipt_to_existing_account():
     # 4.4.2 Existing User Receipt
     assert emailUtils.account_exist(
         EXISTING_ACCOUNT_EMAIL) == True, 'Account does exist'
@@ -56,33 +56,33 @@ def test_send_receipt_to_existing_account(self):
         EXISTING_ACCOUNT_EMAIL, order_receipt) == True, 'Email has been sent'
 
 
-def test_email_not_found(self):
+def test_email_not_found():
     # 4.5.1.1 Email is not found
     assert emailUtils.get_customer_information(
         GUEST_EMAIL) == False, 'Can not find customer information'
 
 
-def test_email_is_found(self):
+def test_email_is_found():
     # 4.5.1.1 Email is found
     assert emailUtils.get_customer_information(
         EXISTING_ACCOUNT_EMAIL) == True, 'Customer information found'
 
 
-def test_valid_shipping_address(self):
+def test_valid_shipping_address():
     # 4.5.1.3 Shipping Address Validation
     # Should there be a failing path?
     assert emailUtils.validate_shipping_address(
         EXISTING_ACCOUNT.address) == True, 'Shipping Address is valid'
 
 
-def test_valid_payment_address(self):
+def test_valid_payment_address():
     # 4.5.1.4 Payment Address Validation
     # Should there be a failing path?
     assert emailUtils.validate_payment_address(
         EXISTING_ACCOUNT.address) == True, 'Payment Address is valid'
 
 
-def test_receipt_saved(self):
+def test_receipt_saved():
     # 4.6.1.2 Receipt Database Storage
     new_order = Order(EXISTING_ACCOUNT, STAFF_ACCOUNT)
 
@@ -92,7 +92,7 @@ def test_receipt_saved(self):
         new_receipt) == True, 'Receipt has been saved'
 
 
-def test_payment_detail_saved(self):
+def test_payment_detail_saved():
     # 4.6.2 Payment Details
     # Possibly 4.6.3 as well?
     new_order = Order(EXISTING_ACCOUNT, STAFF_ACCOUNT)
@@ -101,13 +101,13 @@ def test_payment_detail_saved(self):
         new_order) == True, 'Payment Information has been saved'
 
 
-def test_customer_can_view_account(self):
+def test_customer_can_view_account():
     # 4.7.1 Personal Account Information
 
     assert accountManger.get_account(EXISTING_ACCOUNT) == True, 'Account found'
 
 
-def test_view_specific_account_data(self):
+def test_view_specific_account_data():
     # 4.7.1.1
 
     # assert accountInformation == {'first_name': 'John', 'last_name': 'Doe',
